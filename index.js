db329e171599eb3396c4d6555d35e30eb1705877
Pereira <rbipp010@170.250.106.110.hwccustomers.com>
2020-08-16 18:31:28 -0400

    added tracking ids to markup

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const PORT = process.env.PORT || 3000;
 const express = require('express');
 const app = express();
-app.use(express.static('public'));
+app.use(express.static('Public'));
 
 app.listen(PORT, () =>
   console.log(`Server is up and running at port ${PORT} 🚀`)
