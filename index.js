d48427bfb51e52a537dfecf11b4dfc6aec7c7a76
Pereira <rbipp010@BKUSMAC-1NT8P3XY.local>
2020-08-16 23:43:12 -0400

    store clicks in the database TRIAL

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
 const PORT = process.env.PORT || 3000;
 const express = require('express');
 const app = express();
+const pg = require('pg');
+const db = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+
+
 app.use(express.static('Public'));
 app.use(express.json());
 
 app.post('/clicks', async function(request, response) {
   const allInfo = request.body;
   console.log(allInfo.id);
-   
+
   const result = await db.query(
-  `INSERT INTO clicks (clickX, clickY, tracking, target, time, id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;`,
-  [
-  allInfo.clickX,
-  allInfo.clickY,
-  allInfo.tracking,
-  allInfo.target,
-  allInfo.time,
-  allInfo.id,
-  ]
-  );
-  console.log(result);
+    `INSERT INTO clicks (clickX, clickY, tracking, target, time, id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;`,
+    [
+    allInfo.clickX,
+    allInfo.clickY,
+    allInfo.tracking,
+    allInfo.target,
+    allInfo.time,
+    allInfo.id,
+    ]
+    );
+    console.log(result);
    
   response.json({ click: 'tracked' });
 });
 
+db.query(`
+  CREATE TABLE IF NOT EXISTS clicks(
+  id SERIAL PRIMARY KEY,
+  time int NOT NULL,
+  target VARCHAR(200) NOT NULL,
+  tracking VARCHAR(200) NOT NULL,
+  clickY VARCHAR(200) NOT NULL,
+  clickX VARCHAR(200) NOT NULL,
+  userId int NOT NULL
+  );
+`);
+
+ 
 app.listen(PORT, () =>
   console.log(`Server is up and running at port ${PORT} 🚀`)
 );
